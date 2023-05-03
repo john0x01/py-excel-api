2e6552bdd72811b77e10ae05b498e6da6a6ac0af
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2023-05-03 14:11:40 -0300

    formatting excel

---
+++ api/controllers/fileController.py
@@ -0,0 +1,36 @@
+from flask import send_file
+import pandas as pd
+import xlsxwriter
+import io
+
+
+def export_excel(response):
+    df = pd.DataFrame(data=response["data"])
+
+    excel_file = io.BytesIO()
+
+    # Escreve o DataFrame no arquivo Excel
+    with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Relatório')
+        workbook = writer.book
+        worksheet = writer.sheets["Relatório"]
+
+        currency_format = workbook.add_format({"num_format": "R$#.##0"})   
+        index = 0     
+        for row_to_format in response["currencyFormat"]:
+            for key in response["data"][0]:
+                if(key == row_to_format):
+                    worksheet.set_column(index, index, 24, currency_format)
+                else:
+                    worksheet.set_column(index, index, 24)
+                index += 1
+                    
+
+    # Retorna o arquivo Excel em memória como um anexo
+    excel_file.seek(0)
+    return send_file(
+        excel_file,
+        attachment_filename='data.xlsx',
+        as_attachment=True,
+        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
--- api/views.py
+++ api/views.py
@@ -1,28 +1,11 @@
-from flask import Blueprint, jsonify, request, send_file
-import pandas as pd
-import xlsxwriter
-import io
+from flask import Blueprint, request
+import json
+from api.controllers.fileController import export_excel
 
 api_bp = Blueprint('api', __name__)
 
-@api_bp.route('/export', methods=['POST'])
+@api_bp.route('/exportDefault', methods=['POST'])
 def post_json():
-    data = request.get_json()
-    data_list = [data]
-    df = pd.DataFrame(data_list)
-    excel_file = io.BytesIO()
+    response = request.get_json()
 
-    # Escreve o DataFrame no arquivo Excel
-    with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
-        df.to_excel(writer, index=False)
-
-    # Retorna o arquivo Excel em memória como um anexo
-    excel_file.seek(0)
-    return send_file(
-        excel_file,
-        attachment_filename='data.xlsx',
-        as_attachment=True,
-        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    )
-
-    # return send_file(output, attachment_filename='output.xlsx', as_attachment=True)
+    return export_excel(response)
