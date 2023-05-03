0bf8647d62e0a054cccc3e9a676d0dacdb719d21
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2023-05-03 08:25:37 -0300

    route & download

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     from api.views import api_bp
     app.register_blueprint(api_bp)
--- api/views.py
+++ api/views.py
@@ -1,8 +1,28 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, send_file
+import pandas as pd
+import xlsxwriter
+import io
 
 api_bp = Blueprint('api', __name__)
 
 @api_bp.route('/export', methods=['POST'])
 def post_json():
     data = request.get_json()
-    return jsonify(data) 
+    data_list = [data]
+    df = pd.DataFrame(data_list)
+    excel_file = io.BytesIO()
+
+    # Escreve o DataFrame no arquivo Excel
+    with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False)
+
+    # Retorna o arquivo Excel em memória como um anexo
+    excel_file.seek(0)
+    return send_file(
+        excel_file,
+        attachment_filename='data.xlsx',
+        as_attachment=True,
+        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+
+    # return send_file(output, attachment_filename='output.xlsx', as_attachment=True)
