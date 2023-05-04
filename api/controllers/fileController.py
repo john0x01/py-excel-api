f3f5e7edb1c5e2246a17b9f91c4723a23702b09e
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2023-05-04 09:29:18 -0300

    feat(api) add "/" route and exception handling

--- api/controllers/fileController.py
+++ api/controllers/fileController.py
@@ -5,32 +5,36 @@ import io
 
 
 def export_excel(response):
-    df = pd.DataFrame(data=response["data"])
+    try:
+        df = pd.DataFrame(data=response["data"])
+        
+        excel_file = io.BytesIO()
 
-    excel_file = io.BytesIO()
+        # Escreve o DataFrame no arquivo Excel
+        with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False, sheet_name='Relatório')
+            workbook = writer.book
+            worksheet = writer.sheets["Relatório"]
 
-    # Escreve o DataFrame no arquivo Excel
-    with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
-        df.to_excel(writer, index=False, sheet_name='Relatório')
-        workbook = writer.book
-        worksheet = writer.sheets["Relatório"]
+            currency_format = workbook.add_format({"num_format": "R$#.##0"})   
+            index = 0     
+            for row_to_format in response["currencyFormat"]:
+                for key in response["data"][0]:
+                    if(key == row_to_format):
+                        worksheet.set_column(index, index, 24, currency_format)
+                    else:
+                        worksheet.set_column(index, index, 24)
+                    index += 1
+                        
 
-        currency_format = workbook.add_format({"num_format": "R$#.##0"})   
-        index = 0     
-        for row_to_format in response["currencyFormat"]:
-            for key in response["data"][0]:
-                if(key == row_to_format):
-                    worksheet.set_column(index, index, 24, currency_format)
-                else:
-                    worksheet.set_column(index, index, 24)
-                index += 1
-                    
-
-    # Retorna o arquivo Excel em memória como um anexo
-    excel_file.seek(0)
-    return send_file(
-        excel_file,
-        attachment_filename='data.xlsx',
-        as_attachment=True,
-        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    )
+        # Retorna o arquivo Excel em memória como um anexo
+        excel_file.seek(0)
+        
+        return send_file(
+            excel_file,
+            attachment_filename='data.xlsx',
+            as_attachment=True,
+            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+    except:
+        return {'message': 'Erro na requisição JSON: Informe "data[]" e "currencyFormat[]" '}, 400
--- api/views.py
+++ api/views.py
@@ -4,8 +4,16 @@ from api.controllers.fileController import export_excel
 
 api_bp = Blueprint('api', __name__)
 
+@api_bp.route('/', methods=['GET'])
+def get():
+    return {'message': 'OK'}, 200
+
 @api_bp.route('/exportDefault', methods=['POST'])
 def post_json():
-    response = request.get_json()
+    try:
+        response = request.get_json()
+    except:
+        return {'message': 'Falha na requisição JSON'}, 400
 
     return export_excel(response)
+
