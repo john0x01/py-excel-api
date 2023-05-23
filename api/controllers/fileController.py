7abe14202b992794fab574abf3ca9000d68b0b62
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2023-05-23 09:06:54 -0300

    patch(fileController) fix column formatting v2

--- api/controllers/fileController.py
+++ api/controllers/fileController.py
@@ -52,25 +52,34 @@ def export_tabs(response):
         # Escreve o DataFrame no arquivo Excel
         with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
             for df in data_frame_array:
-                sheet_name = response[counter]["title"] if response[counter]["title"] else 'Dados ' + str(counter + 1)
+                try: 
+                    sheet_name = response[counter]["title"] if response[counter]["title"] else 'Dados ' + str(counter + 1)
+                except:
+                    sheet_name = 'Dados'+ str(counter + 1)
+                
                 df.to_excel(writer, index=False, sheet_name=sheet_name)
                 workbook = writer.book
                 worksheet = writer.sheets[sheet_name]
 
                 currency_format = workbook.add_format({"num_format": "R$0.00"})  
-                print(f"Formatos {response[counter]['currencyFormat']}") 
-                for row_to_format in response[counter]["currencyFormat"]:
-                    print(row_to_format)
-                    index = 0     
-                    for key in response[counter]["data"][0]:
-                        print(f"Analisando coluna {key}")
-                        if(key == row_to_format):
-                            print(f"É igual a {row_to_format}")
-                            worksheet.set_column(index, index, 24, currency_format)
-                        else:
-                            print(f"Não é igual a {row_to_format}")
-                            worksheet.set_column(index, index, 24)
-                        index += 1
+                for column in response[counter]["data"][0]:
+                    print(f"Analisando coluna {column}")
+                    if(column in response[counter]["currencyFormat"]):
+                        worksheet.set_column(index, index, 24, currency_format)
+                    index += 1
+                # print(f"Formatos {response[counter]['currencyFormat']}") 
+                # for row_to_format in response[counter]["currencyFormat"]:
+                #     print(row_to_format)
+                #     index = 0     
+                #     for key in response[counter]["data"][0]:
+                #         print(f"Analisando coluna {key}")
+                #         if(key == row_to_format):
+                #             print(f"É igual a {row_to_format}")
+                #             worksheet.set_column(index, index, 24, currency_format)
+                #         else:
+                #             print(f"Não é igual a {row_to_format}")
+                #             worksheet.set_column(index, index, 24)
+                #         index += 1
                 counter = counter + 1
                         
 
