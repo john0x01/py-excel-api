ab5dbd73be50fae76d457e6eb2c7597aa6c8c648
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2023-05-26 08:55:56 -0300

    debug addTitle

--- api/controllers/fileController.py
+++ api/controllers/fileController.py
@@ -90,17 +90,17 @@ def export_tabs(response):
                         worksheet.set_column(index, index, 24)
                     index += 1
 
-                if has_title:
-                    page_title = response[counter]["header"]["title"]
-                    worksheet.write(0, 0, page_title, title_format)
-                    worksheet.merge_range(0, 0, 0, index, page_title, title_format)
+                # if has_title:
+                #     page_title = response[counter]["header"]["title"]
+                #     worksheet.write(0, 0, page_title, title_format)
+                #     worksheet.merge_range(0, 0, 0, index, page_title, title_format)
 
-                # if has_date:
-                #     page_date = response[counter]["header"]["date"]
-                #     worksheet.write(1, 0, "PERÍODO DE PROGRAMAÇÃO", title_format)
-                #     worksheet.write(2, 0, page_date, title_format)
-                #     worksheet.merge_range(1, 1, 0, 1, "PERÍODO DE PROGRAMAÇÃO", title_format)                        
-                #     worksheet.merge_range(2, 2, 0, 1, page_date, title_format)    
+                if has_date:
+                    page_date = response[counter]["header"]["date"]
+                    worksheet.write(1, 0, "PERÍODO DE PROGRAMAÇÃO", title_format)
+                    worksheet.write(2, 0, page_date, title_format)
+                    worksheet.merge_range(1, 1, 0, 1, "PERÍODO DE PROGRAMAÇÃO", title_format)                        
+                    worksheet.merge_range(2, 2, 0, 1, page_date, title_format)    
                 counter = counter + 1
 
                         
